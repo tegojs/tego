60a2167eeb22cdfba066d0ba6f57ff1d1b4fca12
bai.zixv <bai.zixv@foxmail.com>
2025-07-25 15:31:32 +0800

    feat: add feishu user manual plugin

---
+++ packages/plugin-user-manual-feishu/src/client/UserManualLink.tsx
@@ -0,0 +1,29 @@
+import { Icon, useToken } from '@tachybase/client';
+
+import { Button, Tooltip } from 'antd';
+
+import { USER_MANUAL_PATH_FEISHU } from './contants';
+import { useTranslation } from './locale';
+
+export const UserManualLink = () => {
+  const { t } = useTranslation();
+  const { token } = useToken();
+  return (
+    <Tooltip title={t('User Manual')}>
+      <Button
+        icon={
+          <Icon
+            type="QuestionCircleOutlined"
+            style={{
+              color: token.colorTextHeaderMenu,
+            }}
+          />
+        }
+        title={t('User Manual')}
+        onClick={() => {
+          window.open(USER_MANUAL_PATH_FEISHU, '_blank');
+        }}
+      />
+    </Tooltip>
+  );
+};
