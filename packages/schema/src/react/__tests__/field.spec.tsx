e7caac49dba83aeb935ae3c73afbd146ac7702ca
sealday <sealday@gmail.com>
2025-07-28 07:25:17 +0800

    chore: remove react-dom in tego

--- packages/schema/src/react/__tests__/field.spec.tsx
+++ packages/schema/src/react/__tests__/field.spec.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { act } from 'react';
 
 import { fireEvent, render, waitFor } from '@testing-library/react';
-import { act } from 'react-dom/test-utils';
 
 import {
   ArrayField,
