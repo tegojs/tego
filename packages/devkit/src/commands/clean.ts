bf95d50ebfcd1761b0d0abf7beb7fd54da8a9270
sealday <sealday@gmail.com>
2025-07-28 17:46:16 +0800

    fix: build failed

---
+++ packages/devkit/src/commands/clean.ts
@@ -0,0 +1,24 @@
+import { Command } from 'commander';
+import { rimrafSync } from 'rimraf';
+
+import { isDev } from '../util';
+
+/**
+ *
+ * @param {Command} cli
+ */
+export default (cli: Command) => {
+  cli
+    .command('clean')
+    .option('--all')
+    .allowUnknownOption()
+    .action((opts) => {
+      if (!isDev()) {
+        return;
+      }
+      rimrafSync('{packages}/*/{lib,esm,es,dist,node_modules}', { glob: true });
+      if (opts.all) {
+        rimrafSync('node_modules', { glob: true });
+      }
+    });
+};
