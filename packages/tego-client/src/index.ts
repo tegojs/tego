ca5cd4411e4c25fbc255a2c7b0eee41097b2a812
张琳 Lin Zhang <sealday@gmail.com>
2025-07-29 04:16:26 +0800

    feat: add tego server/client api (#744)
    * feat: add tego server/client api
    
    * chore(deps): revert dotenv to 16.6.1 to avoid annoying ads

---
+++ packages/tego-client/src/index.ts
@@ -0,0 +1,68 @@
+export { APIClient, getSubAppName } from '@tachybase/sdk';
+export type { APIClientOptions, IResource } from '@tachybase/sdk';
+export { getRequireJs } from '@tachybase/requirejs';
+export type { RequireJS } from '@tachybase/requirejs';
+
+export {
+  CollectionsGraph,
+  Registry,
+  convertUTCToLocal,
+  dayjs,
+  error,
+  flatten,
+  forEach,
+  fuzzysearch,
+  getDefaultFormat,
+  getScrollParent,
+  getValuesByPath,
+  isArray,
+  isPlainObject,
+  isPortalInBody,
+  isString,
+  isURL,
+  lodash,
+  merge,
+  moment2str,
+  nextTick,
+  parse,
+  str2moment,
+  toFixedByStep,
+  toGmt,
+  toLocal,
+  uid,
+  unflatten,
+} from '@tachybase/utils/client';
+export type { Dayjs } from '@tachybase/utils/client';
+export {
+  ArrayBase,
+  ArrayCollapse,
+  ArrayItems,
+  ArrayTable,
+  Checkbox,
+  CodeEditor,
+  CodeMirror,
+  DatePicker,
+  Editable,
+  Form,
+  FormButtonGroup,
+  FormCollapse,
+  FormDrawer,
+  FormItem,
+  FormLayout,
+  FormTab,
+  Input,
+  Lightbox,
+  NumberPicker,
+  Radio,
+  Reset,
+  Space,
+  Submit,
+  Switch,
+  TreeSelect,
+  dayjsable,
+  formatDayjsValue,
+  useFormLayout,
+  usePrefixCls,
+} from '@tachybase/components';
+export { evaluate, evaluators, getOptions } from '@tachybase/evaluators/client';
+export type { Evaluator } from '@tachybase/evaluators/client';
