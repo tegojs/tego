f2c25dd8d093c162c4d35a2db0962ef59d9e31c9
sealday <sealday@gmail.com>
2025-08-05 07:27:28 +0800

    fix: .env files generated

--- packages/devkit/src/commands/postinstall.ts
+++ packages/devkit/src/commands/postinstall.ts
@@ -15,14 +15,5 @@ export default (cli: Command) => {
       if (!isDev()) {
         return;
       }
-      const cwd = process.cwd();
-      if (!existsSync(resolve(cwd, '.env')) && existsSync(resolve(cwd, '.env.example'))) {
-        const content = await readFile(resolve(cwd, '.env.example'), 'utf-8');
-        await writeFile(resolve(cwd, '.env'), content, 'utf-8');
-      }
-      if (!existsSync(resolve(cwd, '.env.test')) && existsSync(resolve(cwd, '.env.test.example'))) {
-        const content = await readFile(resolve(cwd, '.env.test.example'), 'utf-8');
-        await writeFile(resolve(cwd, '.env.test'), content, 'utf-8');
-      }
     });
 };
