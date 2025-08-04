c7a92891ac2b79b65f39ac69e5eab29cdf5e317f
sealday <sealday@gmail.com>
2025-08-04 20:45:58 +0800

    feat: init with index.tego.json

--- packages/tego/src/constants.ts
+++ packages/tego/src/constants.ts
@@ -9,3 +9,5 @@ export const DEFAULT_DEV_PLUGINS_PATH = path.join(process.env.TEGO_RUNTIME_HOME,
 export const DEFAULT_REMOTE_PLUGINS_PATH = path.join(process.env.TEGO_RUNTIME_HOME, 'plugins', 'remote');
 export const DEFAULT_BUILTIN_PLUGINS_PATH = path.join(process.env.TEGO_RUNTIME_HOME, 'plugins', 'builtin');
 export const DEFAULT_WEB_PACKAGE_NAME = '@tego/web';
+export const INDEX_TEGO_URL = 'https://tachybase.org/index.tego.json';
+export const LAST_UPDATE_FILE_SUFFIX = '.last-update-at';
