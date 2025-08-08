580c7230bec7518f6d14bf3c3ce4092c5c94caeb
sealday <sealday@gmail.com>
2025-08-08 21:13:58 +0800

    test: fix coverage config

--- packages/test/vitest.ts
+++ packages/test/vitest.ts
@@ -48,6 +48,10 @@ export default defineConfig({
   test: {
     coverage: {
       provider: 'v8',
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ['text', 'json-summary', 'json'],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
     },
     silent: !!process.env.GITHUB_ACTIONS,
     globals: true,
