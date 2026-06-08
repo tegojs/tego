3c485ae9b1ef2acd7af92c1671b1a45310a36e98
TomyJan <TomyJan6@gmail.com>
2026-06-08 18:17:18 +0800

    fix(client): 恢复客户端组件统一导出

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export { APIClient, getSubAppName } from '@tachybase/sdk';
 export type { APIClientOptions, IResource } from '@tachybase/sdk';
 export { getRequireJs } from '@tachybase/requirejs';
@@ -32,59 +30,39 @@ export {
   uid,
   unflatten,
 } from '@tachybase/utils/client';
-export type { ArrayBaseMixins, IArrayBaseAdditionProps } from '@tachybase/components/lib/array-base';
+export {
+  type ArrayBaseMixins,
+  ArrayBase,
+  ArrayCollapse,
+  ArrayItems,
+  ArrayTable,
+  Checkbox,
+  CodeEditor,
+  CodeMirror,
+  DatePicker,
+  Editable,
+  Form,
+  FormButtonGroup,
+  FormCollapse,
+  FormDrawer,
+  FormItem,
+  FormLayout,
+  FormTab,
+  Input,
+  Lightbox,
+  NumberPicker,
+  Radio,
+  Reset,
+  Space,
+  Submit,
+  Switch,
+  TreeSelect,
+  dayjsable,
+  formatDayjsValue,
+  useFormLayout,
+  usePrefixCls,
+  type IArrayBaseAdditionProps,
+} from '@tachybase/components';
 export type { ReactFC } from '@tachybase/schema';
 export { evaluate, evaluators, getOptions } from '@tachybase/evaluators/client';
 export type { Evaluator } from '@tachybase/evaluators/client';
-
-const lazyComponent = (modulePath: string, exportName: string) => {
-  let cached: any;
-  const load = () => {
-    if (!cached) {
-      cached = require(modulePath)[exportName];
-    }
-    return cached;
-  };
-  const Component = React.forwardRef<any, any>((props, ref) => React.createElement(load(), { ...props, ref }));
-  return new Proxy(Component, {
-    get(target, prop, receiver) {
-      if (prop in target) {
-        return Reflect.get(target, prop, receiver);
-      }
-      return load()?.[prop];
-    },
-  });
-};
-
-export const dayjsable = (...args: any[]) => require('@tachybase/components/lib/__builtins__').dayjsable(...args);
-export const formatDayjsValue = (...args: any[]) =>
-  require('@tachybase/components/lib/__builtins__').formatDayjsValue(...args);
-export const usePrefixCls = (...args: any[]) => require('@tachybase/components/lib/__builtins__').usePrefixCls(...args);
-export const useFormLayout = (...args: any[]) =>
-  require('@tachybase/components/lib/form-layout').useFormLayout(...args);
-
-export const ArrayBase = lazyComponent('@tachybase/components/lib/array-base', 'ArrayBase');
-export const ArrayCollapse = lazyComponent('@tachybase/components/lib/array-collapse', 'ArrayCollapse');
-export const ArrayItems = lazyComponent('@tachybase/components/lib/array-items', 'ArrayItems');
-export const ArrayTable = lazyComponent('@tachybase/components/lib/array-table', 'ArrayTable');
-export const Checkbox = lazyComponent('@tachybase/components/lib/checkbox', 'Checkbox');
-export const CodeEditor = lazyComponent('@tachybase/components/lib/code-mirror', 'CodeEditor');
-export const CodeMirror = lazyComponent('@tachybase/components/lib/code-mirror', 'CodeMirror');
-export const DatePicker = lazyComponent('@tachybase/components/lib/date-picker', 'DatePicker');
-export const Editable = lazyComponent('@tachybase/components/lib/editable', 'Editable');
-export const Form = lazyComponent('@tachybase/components/lib/form', 'Form');
-export const FormButtonGroup = lazyComponent('@tachybase/components/lib/form-button-group', 'FormButtonGroup');
-export const FormCollapse = lazyComponent('@tachybase/components/lib/form-collapse', 'FormCollapse');
-export const FormDrawer = lazyComponent('@tachybase/components/lib/form-drawer', 'FormDrawer');
-export const FormItem = lazyComponent('@tachybase/components/lib/form-item', 'FormItem');
-export const FormLayout = lazyComponent('@tachybase/components/lib/form-layout', 'FormLayout');
-export const FormTab = lazyComponent('@tachybase/components/lib/form-tab', 'FormTab');
-export const Input = lazyComponent('@tachybase/components/lib/input', 'Input');
-export const Lightbox = lazyComponent('@tachybase/components/lib/lightbox', 'Lightbox');
-export const NumberPicker = lazyComponent('@tachybase/components/lib/number-picker', 'NumberPicker');
-export const Radio = lazyComponent('@tachybase/components/lib/radio', 'Radio');
-export const Reset = lazyComponent('@tachybase/components/lib/reset', 'Reset');
-export const Space = lazyComponent('@tachybase/components/lib/space', 'Space');
-export const Submit = lazyComponent('@tachybase/components/lib/submit', 'Submit');
-export const Switch = lazyComponent('@tachybase/components/lib/switch', 'Switch');
-export const TreeSelect = lazyComponent('@tachybase/components/lib/tree-select', 'TreeSelect');
